ac29b41e23838612607926bb73dd3b922235a4d8
Felix Bidossessi AGOUNTOHOZIN <bidossessifelixaagountohozin@gmail.com>
2026-03-10 12:33:52 +0100

    Le rendu du cp1

---
+++ script.js
@@ -0,0 +1,10 @@
+//Ajouter une tâche
+let form = document.querySelector("form")
+let btn = document.querySelector("button")
+btn.addEventListener("submit", (e)=> {
+    e.preventDefault()
+let link = document.querySelector("a").value
+link.push()
+})
+//supprimer une tache
+deleteTask()
